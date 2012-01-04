8a4f49262fa5ce8dc2627f308fcfb941e820bd5b
Jari Aalto <jari.aalto@cante.net>
2012-01-04 15:04:33 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/psvn-epkg-0loaddefs.el
@@ -0,0 +1,26 @@
+
+;;;### (autoloads (svn-status svn-checkout) "../psvn" "../psvn.el"
+;;;;;;  (20228 18048))
+;;; Generated autoloads from ../psvn.el
+
+(autoload 'svn-checkout "../psvn" "\
+Run svn checkout REPOS-URL PATH.
+
+\(fn REPOS-URL PATH)" t nil)
+ (defalias 'svn-examine 'svn-status)
+
+(autoload 'svn-status "../psvn" "\
+Examine the status of Subversion working copy in directory DIR.
+If ARG is -, allow editing of the parameters. One could add -N to
+run svn status non recursively to make it faster.
+For every other non nil ARG pass the -u argument to `svn status', which
+asks svn to connect to the repository and check to see if there are updates
+there.
+
+If there is no .svn directory, examine if there is CVS and run
+`cvs-examine'. Otherwise ask if to run `dired'.
+
+\(fn DIR &optional ARG)" t nil)
+
+;;;***
+(provide 'psvn-epkg-0loaddefs)
